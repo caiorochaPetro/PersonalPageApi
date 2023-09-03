47e89fc7c89104442c511053429585d978042dd1
caiorochaPetro <tenshimkai@gmail.com>
2023-09-03 11:37:31 -0300

    Creating struxtures to provide initial funtionalites as login sys, user, and posts

---
+++ index.js
@@ -0,0 +1,302 @@
+import { ApolloServer } from 'apollo-server';
+import {gql} from 'apollo-server';
+import { config } from 'dotenv';
+import { Sequelize, DataTypes} from 'sequelize';
+
+import jwt from 'jsonwebtoken';
+
+
+config();
+
+const sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      dialect: 'postgres', // Especifique o dialeto do PostgreSQL
+      logging: true, // Desabilite o log das queries SQL (opcional)
+    }
+  );
+  
+  // Teste a conexão
+
+sequelize.authenticate().then(() => {
+  console.log('Conexão com o banco de dados estabelecida com sucesso.');
+}).catch((error) => {
+  console.error('Erro ao conectar-se ao banco de dados:', error);
+});
+
+
+//Definition
+const Post = sequelize.define('post', {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    status: { type: DataTypes.STRING },
+    link: { type: DataTypes.STRING },
+    title: { type: DataTypes.STRING },
+    short: {type: DataTypes.STRING},
+    content: { type: DataTypes.STRING },
+    createdAt: { type: DataTypes.DATE },
+    updatedAt: { type: DataTypes.DATE },
+  },{
+    tableName: 'post',schema:'public', name:{singular:'post', plural:'posts'}, 
+  });
+
+const User = sequelize.define('user', {
+    id: {type: DataTypes.INTEGER, primaryKey: true},
+    name: {type: DataTypes.STRING},
+    pass:{type: DataTypes.STRING},
+    email: {type: DataTypes.STRING},
+    createdAt: {type: DataTypes.STRING},
+    updatedAt:{type: DataTypes.STRING}
+  },{
+    tableName: "user"
+  });
+
+sequelize.sync();
+
+// Resources
+//1 
+
+async function verifyToken (token){
+  //console.log("AQUIIIIIIIIIIIIII: "+ JSON.stringify(token.user.name));
+  let name = token.user.name;
+  let user = await User.findOne({
+    where:{
+      name: name
+    }
+  })
+  if (user!==null){
+    return true
+  }
+  else{
+    return false
+  }
+}
+
+//Relations
+
+async function resolver_getAllPosts(){
+    let data = await Post.findAll();
+    console.log(data);
+    return data;
+}
+
+async function resolver_getOnePost(args){
+  let id = args;
+  let data = await Post.findByPk(id);
+  return data;
+}
+
+//Backend
+
+const resolvers = {
+  Query: {
+      getAllPosts: async () => {
+        let data = resolver_getAllPosts();
+        return data;
+      },
+      getOnePost: async (_,args) =>{
+        const {id} = args;
+        let data = await resolver_getOnePost(id);
+        return data;
+      },
+      getOneUser: async (_, {input})=>{
+        let {name, pass} = input;
+        try{
+        let user = await User.findOne({
+          where:{
+
+              name: name,
+              pass: pass
+
+          }
+        });
+        if(user != null){
+          console.log(user.name);
+          const SECRET_KEY = process.env.SECRET_KEY; 
+          const token = jwt.sign({ user }, SECRET_KEY, { expiresIn: '1h' });
+          return {token} ;
+        }
+        else{
+          return null;
+        }
+      }
+      catch(error){
+        throw error;
+      }
+      }
+  },
+  Mutation: {
+      setPost: async (_, { input }, context) => {
+        //console.log("AQUIIIIIIIIIIIIIIIIIIII"+context.user);
+        try{
+          if (verifyToken(context.user)){
+            const { title, status, link, content, short, createdAt, updatedAt } = input;
+            const newPost = await Post.create({
+                status,
+                link,
+                title,
+                short,
+                content,
+                createdAt,
+                updatedAt,
+            });
+            return newPost;
+          }
+          else{
+            return "Invalid Token"
+          }
+        }
+      catch(e){
+        throw e;
+      }
+
+      },
+      updatePost: async (_, {input}, context)=>{
+        try{
+          if (verifyToken(context.user)){
+            const {id, title,status, link, content, short} = input;
+            const post = await Post.findByPk(id);
+            post.title = title;
+            post.status = status;
+            post.short = short;
+            post.link = link;
+            post.content = content;
+
+            await post.save();
+            return post;
+            //}
+            //else throw "deu não";
+          }
+          else{
+            return "Invalid Token"
+          }
+        }
+        catch(error){
+          throw error;
+        }
+      
+      },
+            
+      deleteOnePost: async (_, input, context)=>{
+        try{
+          if (verifyToken(context.user)){
+            let {id} = input;
+            let object = await Post.findByPk(id);
+            object.destroy({
+                where: { id: id },
+            });
+              return object;
+            }
+          else{
+              return "Invalid Token"
+            }
+          }
+          catch(error){
+            return error
+          }
+        }
+    }
+  
+  }
+        
+const typeDefs = gql`
+    scalar Date
+    type post {
+        id: Int
+        status: String
+        link: String
+        title: String
+        short: String
+        content: String
+        createdAt: Date
+        updatedAt: Date
+    }
+
+    type user {
+      id: Int
+      pass: String
+      name: String
+      email: String
+      token: String
+      createdAt: Date
+      updatedAt: Date
+
+    }
+
+    type idPick{
+      id: Int
+    }
+
+    input passName{
+      name: String
+      pass: String
+    }
+
+    type Query {
+        getAllPosts: [post]
+        getOnePost(id: Int): post
+        getOneUser(input: passName): user
+        
+    }
+    input inPost{
+        status: String
+        link: String
+        title: String
+        short: String
+        content: String
+        createdAt: Date
+        updatedAt: Date
+    }
+
+    input inOnePost{
+      id: Int
+      status: String
+      link: String
+      title: String
+      short: String
+      content: String
+      createdAt: Date
+      updatedAt: Date
+  }
+
+    type Mutation{
+        setPost(input: inPost!): post
+        updatePost (input: inOnePost!): post
+        deleteOnePost (id : Int): post
+    }
+`;
+
+const SECRET_KEY = process.env.SECRET_KEY; 
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  playground: true, // Ativa o Playground
+  context: async ({ req, res }) => {
+    // Verifique o token JWT no cabeçalho da solicitação
+    const token = req.headers.authorization || '';
+    try {
+      if (!token) {
+        throw new Error('Token não fornecido');
+      }
+      // Verifique a validade do token JWT e decodifique-o
+      const user =  jwt.verify(token.replace('Bearer ', ''), SECRET_KEY);
+
+      // Se o token for válido, retorne o usuário autenticado no contexto
+      return { user };
+    } catch (error) {
+      // Verifique se o erro é devido ao token expirado
+      if (error.name === 'TokenExpiredError') {
+        return { error: 'Token expirado' };
+      }
+      return { error: 'Token inválido' }; // Outros erros relacionados a JWT
+    }
+  },
+});
+
+server.listen().then(({ url }) => {
+    console.log(`Servidor Apollo pronto em ${url}`);
+  });
